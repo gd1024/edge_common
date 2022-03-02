5d8624177e5de0086e7e27f15d771ad22e37e486
unknown <dongdong.guo@irootech.com>
2022-03-02 15:11:00 +0800

    commit

--- http/http.go
+++ http/http.go
@@ -1,11 +1,15 @@
 package http
 
 import (
+	"bytes"
 	"context"
+	"errors"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"io/ioutil"
 	"net"
 	"net/http"
+	"net/url"
 	"sync"
 	"time"
 )
@@ -52,4 +56,71 @@ func InitHttp(conf Conf, middleware ...gin.HandlerFunc) {
 	err = server.Serve(l)
 	fmt.Println("----- serve close", time.Now().Format("2006-01-02 15:04:05.999999999"), err)
 
+}
+
+type respEntity struct {
+	Code RespCode    `json:"code"`
+	Msg  string      `json:"msg"`
+	Data interface{} `json:"data"`
+}
+
+func RespSuccess(c *gin.Context, data interface{}) {
+	c.JSON(200, respEntity{
+		Code: RespCodeSuccess,
+		Msg:  RespMsg[RespCodeSuccess],
+		Data: data,
+	})
+}
+
+func RespError(c *gin.Context, code RespCode, data interface{}) {
+	c.JSON(200, respEntity{
+		Code: code,
+		Msg:  RespMsg[code],
+		Data: data,
+	})
+}
+
+func BuildParams(params map[string]string) string {
+
+	vals := url.Values{}
+	for k, v := range params {
+		if v == "" {
+			continue
+		}
+		vals.Add(k, v)
+	}
+
+	return vals.Encode()
+}
+
+// 发送普通请求
+func SendRequest(method, url string, param []byte, header map[string]string) ([]byte, error) {
+
+	client := &http.Client{}
+	//req, err := http.NewRequest(method, url, strings.NewReader(postParam))
+	req, err := http.NewRequest(method, url, bytes.NewReader(param))
+	if err != nil {
+		return nil, err
+	}
+	if method == "POST" {
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	}
+	if header != nil {
+		for key, value := range header {
+			req.Header.Set(key, value)
+		}
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		return nil, errors.New("error reponse code:" + string(res.StatusCode))
+	}
+
+	content, err := ioutil.ReadAll(res.Body)
+	return content, err
 }
